ba1dcba1ddb174ca5b53df8ceda7f2461fe51d62
mianjazibali <m.mianjazibali@gmail.com>
2020-01-03 13:06:17 +0500

    Search for city forecast on enter key press

--- src/components/search/input/SearchInput.js
+++ src/components/search/input/SearchInput.js
@@ -5,8 +5,19 @@ let searchInput = (props) => {
     return (  
         <div className="container">
             <div className="container__item">
-                <form className="form">
-                    <input type="email" className="form__field" placeholder="City" onChange={event => props.changeLocation(event)} value={props.location} />
+                <form className="">
+                    <input type="text"
+                        className="form__field"
+                        placeholder="City"
+                        onChange={event => props.changeLocation(event)}
+                        value={props.location}
+                        onKeyPress={event => {
+                            if (event.key === 'Enter') {
+                                props.searchHandler()
+                                event.preventDefault();
+                            }
+                        }} 
+                    />
                     <button type="button" className="btn btn--primary btn--inside uppercase" onClick={props.searchHandler}>Search</button>
                 </form>
             </div>
